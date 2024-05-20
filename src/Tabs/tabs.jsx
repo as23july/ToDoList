fb3513fdcf0711898494d4655945348f7a13de52
Aditya Singh <as23july@gmail.com>
2024-05-20 21:42:42 +0530

    todo list project completed

---
+++ src/Tabs/tabs.jsx
@@ -0,0 +1,17 @@
+export let tabs = [
+  {
+    title: "Summary",
+    content:
+      "This is the content of Tab 1 Your resume summary is your first opportunity to share your goals and aspirations for your career with a hiring manager. It should be short, between two to four sentences, and should focus on specific skills and abilities that you bring to a developer role."
+  },
+  {
+    title: "Exprience",
+    content:
+      "Your experience section should be one of the most fleshed-out parts of your resume. Try only to include experience that applies to the job that you’re applying for. As you build more experience and work in several roles, your resume will be tailored towards your developer and programming roles."
+  },
+  {
+    title: "Education",
+    content:
+      " there are two types of hiring managers within the IT world: 1. Those who pay a lot of attention to what school you went to, and 2. Those who care more about your practical experience. Since you never know what type you are going to run into while applying for roles, it’s best to highlight your university education, as well as any courses and conferences that you’ve attended. This is especially true if you have gone to a prestigious school or completed a specialized course."
+  },
+];
